c746a9940aaa85c63212f7f26a230cfda237e211
浚陶 毛 <1048493621@qq.com>
2021-10-24 09:52:09 +0800

    添加项目文件。

---
+++ SingletonPatternDemo/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SingletonPatternDemo
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void 普通模式ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmCommon frmCommon = new FrmCommon();
+            frmCommon.MdiParent = this;
+            frmCommon.Show();
+        }
+
+        private void 单例模式ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmSingleton frmSingleton = FrmSingleton.CreateForm();
+            frmSingleton.MdiParent = this;
+            frmSingleton.Show();
+        }
+    }
+}
---
+++ SingletonPatternDemo/FrmSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SingletonPatternDemo
+{
+    public partial class FrmSingleton : Form
+    {
+        private static FrmSingleton _frmSingleton;
+
+        public static FrmSingleton CreateForm()
+        {
+            if (_frmSingleton == null || _frmSingleton.IsDisposed == true)
+            {
+                _frmSingleton = new FrmSingleton();
+            }
+            return _frmSingleton;
+        }
+
+        private FrmSingleton()
+        {
+            InitializeComponent();
+        }
+    }
+}
